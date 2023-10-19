f4a0aa16c4bcc339d9de32a71dbf489e6bb5cd79
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-10-19 12:41:12 +0500

    Update the enivronment variables

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import Link from 'next/link'
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
 
 
 const SignupPage = () => {
