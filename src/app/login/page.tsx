039b1810dd997802613e60197a9b4dbfafe2d4c6
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-10-19 12:03:06 +0500

    Make file structures

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div className='text-center text-blue-700 text-3xl font-bold'>Login Page</div>
+  )
+}
+
+export default LoginPage
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignupPage = () => {
+  return (
+    <div className='text-center text-3xl font-bold text-green-500'>Signup Page</div>
+  )
+}
+
+export default SignupPage
