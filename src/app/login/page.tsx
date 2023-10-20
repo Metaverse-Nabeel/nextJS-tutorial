a8b3c5201e982dcd1fddbdda4e0311e5f3a0f68f
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-10-20 12:02:51 +0500

    Add login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,13 @@
+import LoginForm from '@/components/LoginForm'
 import React from 'react'
 
 const LoginPage = () => {
   return (
-    <div className='text-center text-blue-700 text-3xl font-bold'>Login Page</div>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-center text-2xl font-semibold mb-8">Login</h1>
+      <LoginForm />
+    </div>
   )
 }
 
-export default LoginPage
+export default LoginPage;
