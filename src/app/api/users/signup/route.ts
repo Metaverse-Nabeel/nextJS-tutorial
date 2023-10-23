03fbf2c70050698559cd9a78db61f1a41c4368ac
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-10-23 23:27:21 +0500

    Signup form tested with DB

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,44 +1,70 @@
+// Import necessary modules
 import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
 
-// connect the database
+// Establish database connection
 connect();
 
+// Define the POST request handler
+export async function POST(request: NextRequest) {
+  try {
+    // Parse the request body
+    const reqBody = await request.json();
+    const { username, email, password } = reqBody;
 
-export async function POST(request: NextRequest){
-    try {
-        const reqBody= await request.json();
-        const {username, email, password} = reqBody;
-        console.log("Request Body: ",reqBody);
-        
-        //check if user already exist
-        const user = await User.findOne({email})
-        if (user) {
-            return NextResponse.json({error: "User alraedy exists"}, {status: 400})
-        }
+    // Check if the user already exists
+    const user = await User.findOne({ email });
 
-        // hash password
-        const salt = await bcryptjs.genSalt(10);
-        const hashedPassword = await bcryptjs.hash(password, salt);
-        const newUser =  new User({
-            username,
-            email,
-            password: hashedPassword
-        })
-
-        const savedUser = await newUser.save();
-        console.log("Saved User:", savedUser)
-
-        return NextResponse.json({
-            message: "User created successfully",
-            success: true,
-            savedUser 
-        })
-
-
-    } catch (error: any) {
-        return NextResponse.json({error: error.message}, {status: 500})
+    // If the user exists, return an error response
+    if (user) {
+      return new NextResponse({
+        body: JSON.stringify({ error: "User already exists" }),
+        status: 400,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
     }
+
+    // Hash the password
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    // Create a new user
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    // Save the new user to the database
+    const savedUser = await newUser.save();
+
+    // Log the saved user
+    console.log(savedUser);
+
+    // Return a success response
+    return new NextResponse({
+      body: JSON.stringify({
+        message: "User created successfully",
+        success: true,
+        savedUser,
+      }),
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error: any) {
+    // Return an error response
+    return new NextResponse({
+      body: JSON.stringify({ error: error.message }),
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
 }
--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -1,50 +1,58 @@
-'use client'
+"use client";
 // SignupForm.tsx
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input"
-import {useEffect, useState} from 'react';
+import { Input } from "@/components/ui/input";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import axios from "axios";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
+import { ThreeCircles } from "react-loader-spinner";
 
 const SignupForm: React.FC = () => {
-  // const router = useRouter();
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
   const [user, setUser] = useState({
-    username: '',
-    email: '',
-    password: '',
-  })
+    username: "",
+    email: "",
+    password: "",
+  });
   const [buttonDisabled, setButtonDisabled] = useState(false);
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     // Signup logic here
     e.preventDefault();
-
     try {
-      
+      setLoading(true);
+      const response = await axios.post("api/users/signup", user);
+      console.log("Signup Success", response.data);
+      router.push("/login");
     } catch (error: any) {
-      console.log("Signup failed", error.message)
-      toast.error(error.message)
-    }
-    finally {
-
+      console.log("SignUp failed", error.message);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
     }
   };
 
   useEffect(() => {
-    if(user.email.length > 0 && user.password.length > 0 && user.username.length > 0) {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.username.length > 0
+    ) {
       setButtonDisabled(false);
     } else {
       setButtonDisabled(true);
     }
-  }, [user])
-  
+  }, [user]);
 
-  return (
-    <>
+  return !loading ? (
     <form onSubmit={handleSubmit} className="max-w-md mx-auto">
       <div className="mb-4">
-        <label htmlFor="username" className="block text-gray-700 text-sm font-bold mb-2">
+        <label
+          htmlFor="username"
+          className="block text-gray-700 text-sm font-bold mb-2"
+        >
           Username
         </label>
         <Input
@@ -52,11 +60,14 @@ const SignupForm: React.FC = () => {
           id="username"
           placeholder="Enter your username"
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          onChange={(e) => setUser({...user, username:e.target.value})}
+          onChange={(e) => setUser({ ...user, username: e.target.value })}
         />
       </div>
       <div className="mb-4">
-        <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
+        <label
+          htmlFor="email"
+          className="block text-gray-700 text-sm font-bold mb-2"
+        >
           Email
         </label>
         <Input
@@ -64,11 +75,14 @@ const SignupForm: React.FC = () => {
           id="email"
           placeholder="Enter your email"
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          onChange={(e) => setUser({...user, email:e.target.value})}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
         />
       </div>
       <div className="mb-6">
-        <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
+        <label
+          htmlFor="password"
+          className="block text-gray-700 text-sm font-bold mb-2"
+        >
           Password
         </label>
         <Input
@@ -76,19 +90,35 @@ const SignupForm: React.FC = () => {
           id="password"
           placeholder="Enter your password"
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          onChange={(e) => setUser({...user, password:e.target.value})}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
         />
       </div>
-      <Button type="submit" className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${buttonDisabled ? "pointer-events-none bg-gray-500 text-gray-200" : ""}`}>
-      Sign Up
+      <Button
+        type="submit"
+        className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${
+          buttonDisabled ? "pointer-events-none bg-gray-500 text-gray-200" : ""
+        }`}
+      >
+        Sign Up
       </Button>
       <Button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 mx-4 px-4 rounded focus:outline-none focus:shadow-outline">
-        <Link href={'/login'}>Login</Link>
+        <Link href={"/login"}>Login</Link>
       </Button>
     </form>
-  </>
+  ) : (
+    <ThreeCircles
+      height="100"
+      width="100"
+      color="#ADD8E6"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+      ariaLabel="three-circles-rotating"
+      outerCircleColor=""
+      innerCircleColor=""
+      middleCircleColor=""
+    />
   );
-
 };
 
 export default SignupForm;
