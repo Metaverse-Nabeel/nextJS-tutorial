b3e6c5cdbd5e966b473df3ef19f5a31001554701
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-10-23 16:56:55 +0500

    Add user models

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,6 +1,33 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({ });
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please provide an Username"],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide an Email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a Password"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  forgotPasswordToken: String,
+  forgotPasswordTokenExpiry: Date,
+  verifyToken: String,
+  verifyTokenExpiry: Date,
+});
 
 const User = mongoose.models.users || mongoose.model("users", userSchema);
 
