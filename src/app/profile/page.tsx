05b9c0839dc7b0fb3a4494972b24c04603c3a9fe
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-10-26 23:46:55 +0500

    Setup me route for the dynamic pages

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,13 +1,52 @@
-import React from 'react'
+"use client";
+
+import React, {useState} from "react";
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const ProfilePage = () => {
-  return (
-    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
-        <h1>Profile Page</h1>
-        <hr />
-        <p>Profile Page</p>
-    </div>
-  )
-}
+  const [data, setData] = useState("")
+  const router = useRouter();
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logout Successful");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
 
-export default ProfilePage
+  const getUserDetails = async () => {
+    const res = await axios.get('/api/users/me')
+    console.log(res.data.data._id);
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Profile Page</h1>
+      <hr />
+      <p>Profile Page</p>
+      <h2 className="p-4 rounded-lg bg-green-800 text-white">{data === "" ? "Nothing" : <Link href={`/profile/${data}`}>{data}</Link>}</h2>
+      <hr />
+      <Button
+        onClick={logout}
+        className={`bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline`}
+      >
+        Logout
+      </Button>
+      <Button
+        onClick={getUserDetails}
+        className={`bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline`}
+      >
+        Get User Details
+      </Button>
+    </div>
+  );
+};
+
+export default ProfilePage;
